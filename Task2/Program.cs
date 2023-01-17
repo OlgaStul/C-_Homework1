8d858212085c8428f94eb63fe5dff46666eabd5f
Olga <chi89@yandex.ru>
2023-01-17 23:22:01 +0300

    выдает наибольшее из двух чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3   */
+
+Console.Write("Введите первое число: а = ");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+
+Console.Write("Введите второе число: b = ");
+string numberStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numberStr2);
+
+if (a > b)
+{
+    Console.WriteLine($"a = {a} > b = {b} ");
+}
+else if (a == b)
+{
+    Console.WriteLine($"a = {a} = b = {b} ");
+}
+else 
+{
+    Console.WriteLine($"a = {a} < b = {b} ");
+}
